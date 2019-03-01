a6d7b221963ca9e00e0557168a2b70a06750f098
Lin, Ying-Chin <mirror770109@gmail.com>
2019-03-02 01:45:54 +0800

    Add HLS Server

---
+++ Dockerfile
@@ -0,0 +1,25 @@
+FROM ubuntu:18.04
+
+RUN apt update && apt upgrade -y \
+ && apt install -y git wget
+
+ENV GOLANG_VERSION 1.12
+ENV goRelArch linux-amd64
+
+RUN wget https://golang.org/dl/go${GOLANG_VERSION}.${goRelArch}.tar.gz \
+ && tar -C /usr/local -xzf go${GOLANG_VERSION}.${goRelArch}.tar.gz \
+ && rm go${GOLANG_VERSION}.${goRelArch}.tar.gz
+
+ENV GOPATH /go
+ENV PATH $GOPATH/bin:/usr/local/go/bin:$PATH
+
+WORKDIR $GOPATH
+
+RUN go get github.com/gorilla/mux \
+ && go get github.com/influxdata/influxdb1-client \
+ && go get github.com/mirror520/hls \
+ && go install github.com/mirror520/hls
+
+EXPOSE 80
+
+CMD hls
---
+++ main.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"strconv"
+	"text/template"
+	"time"
+
+	"github.com/gorilla/mux"
+	client "github.com/influxdata/influxdb1-client"
+)
+
+// Playlist ...
+type Playlist struct {
+	URL       string
+	Path      string
+	Stream    string
+	Channel   string
+	StartTime time.Time
+	EndTime   time.Time
+	Files     []RecordFile
+}
+
+// RecordFile ...
+type RecordFile struct {
+	Time     time.Time
+	RecordID int
+	Filename string
+}
+
+var influxClient *client.Client
+var location *time.Location
+
+const playlistTemplate = `#EXTM3U
+#EXT-X-PLAYLIST-TYPE:VOD
+#EXT-X-TARGETDURATION:60
+#EXT-X-VERSION:3
+{{range .Files}}#EXTINF:60,
+{{$.URL}}{{$.Path}}/{{.Time | toDate}}/{{.RecordID}}/{{.Filename}}
+{{end}}#EXT-X_ENDLIST`
+
+func toDate(t time.Time) string {
+	return t.Format("2006/01/02")
+}
+
+func playlistHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	start, _ := strconv.ParseInt(vars["start"], 10, 64)
+	end, _ := strconv.ParseInt(vars["end"], 10, 64)
+	startTime := time.Unix(start, 0)
+	endTime := time.Unix(end, 0)
+	files := getRecordFiles(vars["stream"], vars["channel"], startTime, endTime)
+
+	playlist := Playlist{
+		URL:       os.Getenv("URL"),
+		Path:      "/mivs/record",
+		Stream:    vars["stream"],
+		Channel:   vars["channel"],
+		StartTime: startTime,
+		EndTime:   endTime,
+		Files:     files,
+	}
+
+	w.Header().Set("Content-Type", "application/x-mpegURL")
+	report := template.Must(template.New("playlist").
+		Funcs(template.FuncMap{"toDate": toDate}).
+		Parse(playlistTemplate))
+	report.Execute(w, playlist)
+}
+
+func getRecordFiles(stream, channel string, startTime, endTime time.Time) []RecordFile {
+	q := client.Query{
+		Command: fmt.Sprintf(`SELECT *::field 
+							  FROM mivs_record_file 
+							  WHERE stream='%s' AND channel='%s' 
+							  AND time >= '%s' AND time <= '%s'`,
+			stream, channel,
+			startTime.Format(time.RFC3339), endTime.Format(time.RFC3339)),
+		Database: "mivs",
+	}
+
+	files := []RecordFile{}
+	if response, err := influxClient.Query(q); err == nil && response.Error() == nil {
+		for _, row := range response.Results[0].Series[0].Values {
+			_time, _ := time.Parse(time.RFC3339, row[0].(string))
+			_filename := row[1].(string)
+			_id, _ := strconv.Atoi(string(row[2].(json.Number)))
+
+			file := RecordFile{
+				Time:     _time.In(location),
+				RecordID: _id,
+				Filename: _filename,
+			}
+
+			files = append(files, file)
+		}
+	}
+
+	return files
+}
+
+func main() {
+	host, _ := url.Parse(fmt.Sprintf("http://%s:%d", os.Getenv("INFLUXDB_HOST"), 8086))
+
+	conf := client.Config{
+		URL: *host,
+	}
+
+	influxClient, _ = client.NewClient(conf)
+	location, _ = time.LoadLocation("Asia/Taipei")
+
+	router := mux.NewRouter()
+	router.HandleFunc("/vod/{stream}/{channel}/playlist.m3u8", playlistHandler).
+		Queries("start", "{start}", "end", "{end}")
+	router.PathPrefix("/mivs/record/").Handler(http.StripPrefix("/mivs/record", http.FileServer(http.Dir(os.Getenv("RECORD_DIR")))))
+	log.Fatal(http.ListenAndServe(":80", router))
+}
